d5f6660424807b5ab7bffed050840ea3b492495b
hakan albay <hakanalbay2@gmail.com>
2019-03-17 14:20:15 +0300

    Mvc Yapısı

---
+++ alistirma-002/Personel/Controllers/DepartmanController.cs
@@ -0,0 +1,19 @@
+﻿using Personel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Personel.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ActionResult dışardan gelen veri tiplerinin tümünü geri döndürür.(ilk örnek string tipindeydi.)
+            var departman = new Departman() { id = 55, Dep_Ad = "Bilgi Teknolojileri" };
+            return View(departman);
+        }
+    }
+}
---
+++ alistirma-002/Personel/Models/Departman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Personel.Models
+{
+    public class Departman
+    {
+        public int id { get; set; }
+        public string Dep_Ad { get; set; }
+    }
+}
