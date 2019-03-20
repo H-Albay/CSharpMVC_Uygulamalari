50b0b9f6dd313af9ce0abf5484c99be7e46cf015
hakan albay <hakanalbay2@gmail.com>
2019-03-20 16:38:23 +0300

    Departman_Ekle

--- alistirma-007/H-Personel/App_Start/RouteConfig.cs
+++ alistirma-007/H-Personel/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace H_Personel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Departman", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- alistirma-007/H-Personel/Controllers/DepartmanController.cs
+++ alistirma-007/H-Personel/Controllers/DepartmanController.cs
@@ -10,6 +10,7 @@ namespace H_Personel.Controllers
     public class DepartmanController : Controller
     {
         DatabaseEntities db = new DatabaseEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -20,6 +21,26 @@ namespace H_Personel.Controllers
             var model = db.Departman.ToList();
             return View(model);
         }
+        public ActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(/*string Ad*/ Departman departman)
+        {
+            /* 1.ekleme yöntemi fakat çok sayıda değişken eklendiği durumda sıkıntı yaratabilir 
+             * bu yüzden object veri tanımlanmalı (2.yöntem)
+             * var departman = new Departman();
+             departman.Ad = Ad;
+             db.Departman.Add(departman);
+            int result= db.SaveChanges();    */
+
+            db.Departman.Add(departman);
+            db.SaveChanges();
+
+            return RedirectToAction("list","Departman");
+        }
+
     }
-    
+
 }
