7ac6b3516070a289021512e12081821791491fd4
hakan albay <hakanalbay2@gmail.com>
2019-03-20 10:53:14 +0300

    v2

--- alistirma-007/H-Personel/Controllers/DepartmanController.cs
+++ alistirma-007/H-Personel/Controllers/DepartmanController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using H_Personel.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace H_Personel.Controllers
 {
     public class DepartmanController : Controller
     {
-        // GET: Departman
+        DatabaseEntities db = new DatabaseEntities();
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult list()
+        {
+            var model = db.Departman.ToList();
+            return View(model);
+        }
     }
+    
 }
