97f2cc4cddbc072d92336381d27b541d1721eb46
halbay <hakanalbay2@gmail.com>
2019-04-28 14:35:58 +0300

    start
    start PetWorld

---
+++ uygulama-001/PetWorld/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PetWorld.Entity;
+namespace PetWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Products.Where(i=>i.IsApproved && i.IsHome).ToList());  // 
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View(db.Products.Where(i=>i.IsApproved).FirstOrDefault());
+        }
+    }
+}
---
+++ uygulama-001/PetWorld/Entity/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PetWorld.Entity
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("dataConnection")  {
+            //projede varsayılan veritabanını oluşturmaması için kullanılıdı.
+            //connectionStrings dataConnection isimde olanı oluştur
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
