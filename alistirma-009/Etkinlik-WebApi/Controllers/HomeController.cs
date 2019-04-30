b13df044ca469f6ad31e28cb4ac626fc3ae81dec
halbay <hakanalbay2@gmail.com>
2019-04-30 22:50:14 +0300

    MVC_Uyg.

---
+++ alistirma-009/Etkinlik-WebApi/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Etkinlik_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Etkinlik_WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DavetiyeForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DavetiyeForm(DavetiyeModel model)
+        {
+            if (ModelState.IsValid == true)
+            {
+                Veritabani.Add(model);
+                return View("Thanks", model);
+            }
+            return View(model);
+        }
+
+        [ChildActionOnly] //URLden Katilanlar görmeyi engelledik.
+        public ActionResult Katilanlar()
+        {
+            var data = Veritabani.Liste.Where(i => i.KatilmaDurumu == true);
+            return PartialView(data);
+        }
+    }
+}
---
+++ alistirma-009/Etkinlik-WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Etkinlik_WebApi.Global" Language="C#" %>
